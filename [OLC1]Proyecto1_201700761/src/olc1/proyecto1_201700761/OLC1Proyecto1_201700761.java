a5f15a4abd65f7a45c3e7d3a510a7f4e3b68a557
Elder Pum <ElderPum@gmail.com>
2022-03-04 00:19:31 -0600

    Menu terminado, métodos de archivos implementados

---
+++ [OLC1]Proyecto1_201700761/src/olc1/proyecto1_201700761/OLC1Proyecto1_201700761.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package olc1.proyecto1_201700761;
+
+/**
+ *
+ * @author Elder
+ */
+public class OLC1Proyecto1_201700761 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
